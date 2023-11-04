ff00bc88b55cc4eaed68a4f83acf7832cbdba21f
Kirill Terekhin <k1r1llter2004@gmail.com>
2023-11-04 20:07:18 +0300

    task 7

--- warm_up/7_rabbit_learns_geometry.py
+++ warm_up/7_rabbit_learns_geometry.py
@@ -13,3 +13,57 @@
 #
 # Формат вывода
 # Выведите одно число — сторону наибольшего квадрата, заполненного морковками.
+
+#
+# n, m = map(int, input().split())
+#
+# field = []
+# for i in range(n):
+#     field.append(list(map(int, input().split())))
+# size = min(n, m)
+# start_i = 0
+# start_j = 0
+# stop_flag = False
+# while size:
+#     for i in range(start_i, start_i + size):
+#         for j in range(start_j, start_j + size):
+#             if field[i][j] == 0:
+#                 stop_flag = True
+#                 break
+#         if stop_flag:
+#             break
+#     if not stop_flag:
+#         break
+#     else:
+#         start_j += 1
+#         if start_j + size > m:
+#             start_j = 0
+#             start_i += 1
+#             if start_i + size > n:
+#                 start_i = 0
+#                 size -= 1
+#         stop_flag = False
+# print(size)
+
+
+def find_largest_square(field, n, m):
+    if not field or not len(field):
+        return 0
+    dp = [[0 for _ in range(m)] for _ in range(n)]
+    max_size = 0
+    for i in range(n):
+        for j in range(m):
+            dp[i][j] = field[i][j]
+            if i > 0 and j > 0 and field[i][j] == 1:
+                dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
+            if max_size < dp[i][j]:
+                max_size = dp[i][j]
+    return max_size
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    field = []
+    for i in range(n):
+        field.append(list(map(int, input().split())))
+    print(find_largest_square(field, n, m))
