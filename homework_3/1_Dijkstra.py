cfc33aa7f91ae3a4b3fc75ca58e8a373631a1070
Kirill Terekhin <k1r1llter2004@gmail.com>
2023-11-16 18:04:52 +0300

    refracting Dijkstra's algorithm

--- homework_3/1_Dijkstra.py
+++ homework_3/1_Dijkstra.py
@@ -9,6 +9,40 @@
 # Формат вывода
 # Выведите искомое расстояние или -1, если пути между указанными вершинами не существует.
 
+
+def dijkstra(adj_list, start, finish):
+    N = len(adj_list) - 1
+    visited = [False] * (N + 1)
+    visited[0] = True
+    dist = [inf] * (N + 1)
+    dist[start] = 0
+    curr_v = 0
+    while not all(visited):
+        # Выбор непосещенной вершины с минимальным расстоянием
+        min_dist = inf
+        for i, _ in enumerate(dist):
+            if dist[i] < min_dist and not visited[i]:
+                min_dist = dist[i]
+                curr_v = i
+        # Отметка посещения
+        visited[curr_v] = True
+        # Изменение весов
+        for weight, vertex in adjacency_list[curr_v]:
+            if dist[vertex] == inf:
+                dist[vertex] = dist[curr_v] + weight
+            else:
+                dist[vertex] = min(dist[vertex], dist[curr_v] + weight)
+
+        stop_flag = True
+        for i, _ in enumerate(dist):
+            if dist[i] != inf and not visited[i]:
+                stop_flag = False
+                break
+        if stop_flag:
+            break
+    return dist[finish] if dist[finish] != inf else -1
+
+
 N, S, F = map(int, input().split())
 inf = float("Inf")
 
@@ -20,34 +54,5 @@ for i in range(1, N + 1):
         if value > 0:
             adjacency_list[i].append((value, index + 1))
 
-# Алгоритм Дейкстры
-visited = [False]*(N + 1)
-visited[0] = True
-dist = [inf]*(N + 1)
-dist[S] = 0
-curr_v = 0
-while not all(visited):
-    # Выбор непосещенной вершины с минимальным расстоянием
-    min_dist = inf
-    for i, _ in enumerate(dist):
-        if dist[i] < min_dist and not visited[i]:
-            min_dist = dist[i]
-            curr_v = i
-    # Отметка посещения
-    visited[curr_v] = True
-    # Изменение весов
-    for weight, vertex in adjacency_list[curr_v]:
-        if dist[vertex] == inf:
-            dist[vertex] = dist[curr_v] + weight
-        else:
-            dist[vertex] = min(dist[vertex], dist[curr_v] + weight)
-
-    stop_flag = True
-    for i, _ in enumerate(dist):
-        if dist[i] != inf and not visited[i]:
-            stop_flag = False
-            break
-    if stop_flag:
-        break
-print(dist[F] if dist[F] != inf else -1)
+print(dijkstra(adjacency_list, S, F))
 
